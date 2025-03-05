7910c6f0d4a0eb0da19caf3cc9602d5f30a2acf1
iamknownasfesal <mkircal957@gmail.com>
2025-03-06 00:14:34 +0300

    fix: metadata

--- src/helpers/metadata.ts
+++ src/helpers/metadata.ts
@@ -1,8 +1,15 @@
 export async function getMetadata(type: string): Promise<{
   decimals: number;
 }> {
-  const response = await fetch(
-    `https://api.patara.app/metadata/coin-metadata/0x${type}`
-  );
-  return response.json();
+  try {
+    const response = await fetch(
+      `https://api.patara.app/metadata/coin-metadata/0x${type}`
+    );
+    return response.json();
+  } catch (error) {
+    console.error(error);
+    return {
+      decimals: 9,
+    };
+  }
 }
